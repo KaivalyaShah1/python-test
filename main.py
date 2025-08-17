c282d78cb43e9e77cf389416cd86827956a5ff51
KaivalyaShah1 <kaivalyas3@gmail.com>
2025-08-17 12:40:37 +0530

    watchflow

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def subtract_numbers(a: int, b: int) -> int:
     return a - b
 
 
-# main
+# main hi
 if __name__ == "__main__":
     print("Sum:", add_numbers(5, 3))
     print("Difference:", subtract_numbers(5, 3))
